0bd5ef99ae3d5d3ace49404ef1357bcfc9e52540
Rishabh-Kala-2003 <rishabh.kala2003@gmail.com>
2024-02-06 03:23:07 +0530

    project update

--- src/components/projects/Portfolio.jsx
+++ src/components/projects/Portfolio.jsx
@@ -6,7 +6,7 @@ const Portfolio = () => {
   return (
     <section className="portfolio section" id="portfolio">
         <h2 className="section__title">Portfolio</h2>
-        <span className="section__subtitle">Recent Projects</span>
+        <span className="section__subtitle">Some of my Projects</span>
 
         <Projects />
     </section>
