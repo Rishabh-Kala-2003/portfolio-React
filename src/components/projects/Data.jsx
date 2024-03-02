e7f38c038f953cd2afc75258e4db57f97f879689
Rishabh-Kala-2003 <rishabh.kala2003@gmail.com>
2024-03-02 21:39:21 +0530

    edit

--- src/components/projects/Data.jsx
+++ src/components/projects/Data.jsx
@@ -3,38 +3,46 @@ import Project2 from "../../assets/hashtag.png";
 import Project3 from "../../assets/techy.png";
 import Project4 from "../../assets/hotel.png";
 import Project5 from "../../assets/Drum.png";
+import Project6 from "../../assets/project6.png"
 
 export const projectsData = [
     {
         id: 1,
+        image: Project6,
+        title: "Blog-Website",
+        category: "Backend",
+        site:"https://github.com/Rishabh-Kala-2003/Blog-website",
+    },
+    {
+        id: 2,
         image: Project1,
         title: "Keeper-NotesTaking-Website",
         category: "React",
         site:"https://github.com/Rishabh-Kala-2003/Keeper-Notes-Taking-site",
     },
     {
-        id: 2,
+        id: 3,
         image: Project2,
         title: "Hashtag",
         category: "HTML+CSS+JS",
         site:"https://hashtag-society.netlify.app/",
     },
     {
-        id: 3,
+        id: 4,
         image: Project3,
         title: "Techystar-Company-Website",
         category: "React",
         site:"https://techystar-react.vercel.app/",
     },
     {
-        id: 4,
+        id: 5,
         image: Project4,
         title: "Responsive-Hotel-Website",
         category: "HTML+CSS+JS",
         site:"https://hotel-rajmahal.netlify.app/",
     },
     {
-        id: 5,
+        id: 6,
         image: Project5,
         title: "Drum-Kit",
         category: "HTML+CSS+JS",
